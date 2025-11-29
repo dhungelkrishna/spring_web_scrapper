41fbd64e1d5dd3cdc0d96d31b1afd4afe7d172f0
Krishna Dhungel <dhungelkrishna807@gmail.com>
2025-11-29 17:13:20 +0545

    initial setup

---
+++ WebScrapping/src/main/java/com/dhungelkrishna/WebScrapping/WebScrappingApplication.java
@@ -0,0 +1,13 @@
+package com.dhungelkrishna.WebScrapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrappingApplication.class, args);
+	}
+
+}
